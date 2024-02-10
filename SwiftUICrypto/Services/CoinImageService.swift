7c300c6470ec15429f5ecb4d835e287f8969b09a
Ayodeji Ayankola <ayodejiayankola@gmail.com>
2024-02-10 17:02:21 +0100

    implement local storage for images downloaded using file manager and retrieval

--- SwiftUICrypto/Services/CoinImageService.swift
+++ SwiftUICrypto/Services/CoinImageService.swift
@@ -17,12 +17,27 @@ class CoinImageService {
 	
 	private let coin: CoinModel
 	
+	private let fileManger = LocalFileManager.instance
+	
+	private let folderName = "coin_images"
+	
+	private let imageName: String
+	
 	init(coin: CoinModel) {
 		self.coin = coin
+		self.imageName = coin.id
 		getCoinImage()
 	}
 	
-	private func getCoinImage(){
+	private func getCoinImage() {
+		if let savedImage = fileManger.getImage(imageName: imageName, folderName: folderName) {
+			image = savedImage
+		} else {
+			downloadCoinImage()
+		}
+	}
+	
+	private func downloadCoinImage(){
 		guard let url = URL(string: coin.image) else { return }
 		
 		imageSubscription = NetworkingManager.download(url: url)
@@ -30,8 +45,10 @@ class CoinImageService {
 				return UIImage(data: data)
 			})
 			.sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
-				self?.image = returnedImage
-				self?.imageSubscription?.cancel()
+				guard let self = self, let downloadedImage = returnedImage else { return }
+				self.image = returnedImage
+				self.imageSubscription?.cancel()
+				fileManger.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
 			})
 	}
 	
