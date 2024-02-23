b791c465617acb9f2ad89a3ff656f469f9b94f88
Ayodeji Ayankola <ayodejiayankola@gmail.com>
2024-02-23 18:03:36 +0100

    Filtering data based on search bar text

--- SwiftUICrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftUICrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -22,19 +22,43 @@ class HomeViewModel: ObservableObject {
 	private var cancellables = Set<AnyCancellable>()
 	
 	init() {
-//		DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-//			
-//			self.allCoins.append(DeveloperPreview.instance.coin)
-//			self.portfolioCoins.append(DeveloperPreview.instance.coin)
-//		}
+		//		DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+		//
+		//			self.allCoins.append(DeveloperPreview.instance.coin)
+		//			self.portfolioCoins.append(DeveloperPreview.instance.coin)
+		//		}
 		addSubscribers()
 	}
 	
 	func addSubscribers() {
-		self.dataService.$allCoins
-			.sink { [weak self](returnedCoins) in
+		//		self.dataService.$allCoins
+		//			.sink { [weak self](returnedCoins) in
+		//				self?.allCoins = returnedCoins
+		//			}
+		//			.store(in: &cancellables)
+		//
+		//updates all coins
+		$searchText
+			.combineLatest(dataService.$allCoins)
+			.debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+			.map(filterCoins)
+			.sink { [weak self] (returnedCoins) in
 				self?.allCoins = returnedCoins
 			}
 			.store(in: &cancellables)
+		
+	}
+	
+	private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+		guard !text.isEmpty else { return coins}
+		
+		let lowercasedText = text.lowercased()
+		
+		return coins.filter { (coin) -> Bool in
+			return coin.name.lowercased().contains(lowercasedText) ||
+			coin.symbol.lowercased().contains(lowercasedText) ||
+			coin.id.lowercased().contains(lowercasedText)
+		}
+		
 	}
 }
